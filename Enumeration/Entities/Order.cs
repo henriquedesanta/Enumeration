452b1dec7f45effdf3212b2394b9c4c04550239e
Carlos Henrique Marques Gomes <carlos.gomes@fitbank.com.br>
2022-01-05 09:05:10 -0300

    Add project files.

---
+++ Enumeration/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enumeration.Entities.Enums;
+
+namespace Enumeration.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public int MyProperty { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ Enumeration/Program.cs
@@ -0,0 +1,34 @@
+﻿using Enumeration.Entities;
+using Enumeration.Entities.Enums;
+using System;
+
+/*    OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); 
+      OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered"); 
+      
+      OrderStatus os;
+      Enum.TryParse("Delivered", out os);*/
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
